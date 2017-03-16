72066e6150a3afa65f94a5be90f26459871ad809
ON10N <zhyc9de@hotmail.com>
2017-03-16 10:41:30 +0800

    fix

--- src/start.sh
+++ src/start.sh
@@ -6,6 +6,6 @@ nohup redis-server /etc/redis/redis.conf &
 sleep 1
 nohup xvfb-run selenium-standalone start &
 sleep 1
-nohup python3 probe.py &
+nohup python3 $(pwd)/probe.py &
 sleep 1
-nohup python3 server.py &
+nohup python3 $(pwd)/server.py &
